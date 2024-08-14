13eb0f6bca40ed3020c3eec9615ad45b109ab0b3
qyb <qiuyingbo@gmail.com>
2024-08-14 16:00:55 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+将 Drupal 7 的内容通过 "Node export" 模块导出 json 后，进一步将该 json 文件处理成每个 node 存一个目录
+
+内容存储成 txt/html 文件；附件也放在该目录下；alias 和分类标签id 等信息单独存 meta.json
---
+++ parser.py
@@ -0,0 +1,68 @@
+import json
+from datetime import datetime
+import os
+import base64
+
+def mkdir(path):
+    try:
+        os.mkdir(path)
+    except FileExistsError:
+        pass
+
+def mkuser(username):
+    mkdir(f"{username}")
+    mkdir(f"{username}/blog")
+    mkdir(f"{username}/story")
+    mkdir(f"{username}/page")
+
+def process(data):
+    body = data['body']['und'][0]
+    node_id = data['nid']
+    created = datetime.fromtimestamp(int(data['created']))
+    changed = datetime.fromtimestamp(int(data['changed']))
+
+    meta_data = {
+        'format': body['format'],
+        'changed': changed.strftime("%Y-%m-%d %H:%M:%S"),
+        'created': created.strftime("%Y-%m-%d %H:%M:%S"),
+        'nid': node_id,
+    }
+    if data['path'] != False:
+        meta_data['alias'] = data['path']['alias']
+    for key in data.keys():
+        if key.startswith('taxonomy_vocabulary'):
+            taxonomy_vocabulary = data[key]
+            if len(taxonomy_vocabulary) > 0:
+                tids = list(map(lambda foo:foo['tid'], taxonomy_vocabulary['und']))
+                meta_data['taxonomy'] = tids
+
+    created_str = created.strftime("%Y-%m-%d %H-%M")
+    title = data["title"].replace('/', '／') # 全角替换
+    path = f'{data["name"]}/{data["type"]}/{created_str}-{title}'
+    try:
+        os.mkdir(path)
+    except FileExistsError:
+        pass
+    txt_filename = f'{path}/{node_id}.txt'
+    html_filename = f'{path}/{node_id}.html'
+    meta_filename = f'{path}/meta.json'
+    with open(txt_filename, "w") as txt:
+        txt.write(body['value'])
+    with open(html_filename, "w") as html:
+        html.write(body['safe_value'])
+    with open(meta_filename, "w") as meta:
+        json.dump(meta_data, meta)
+    if 'upload' in data and len(data['upload']) > 0:
+        uploads = data['upload']['und']
+        for upload in uploads:
+            blob = base64.b64decode(upload['node_export_file_data'])
+            with open(f'{path}/{upload["filename"]}', "wb") as f:
+                f.write(blob)
+
+if __name__ == '__main__':
+    import sys
+    with open(sys.argv[1]) as file:
+        mkuser('admin')
+        for data in json.load(file):
+            if data['name'] == 'admin':
+                process(data)
